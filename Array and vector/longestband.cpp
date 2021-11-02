d15a6c34e50c44446742f4d519f22ea4b2243247
RITURESH KUMAR RAI <76609761+Rituresh143@users.noreply.github.com>
2021-11-02 12:48:38 +0530

    Create longestband.cpp

---
+++ Array and vector/longestband.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+#include<unordered_set>
+using namespace std;
+
+int largestBand(vector<int> arr){
+
+	int n = arr.size();
+	unordered_set<int> s;
+
+	// Data inside a set
+	for(int x : arr){
+		s.insert(x);
+	}
+
+   // Iterate over the arr
+	int largestLen = 1;
+
+	for(auto element : s){
+		int parent = element - 1;
+
+		if(s.find(parent)==s.end()){
+			// find entire band / chain starting from element
+			int next_no = element + 1;
+			int cnt = 1;
+
+			while(s.find(next_no)!=s.end()){
+				next_no++;
+				cnt++;
+			}
+
+			if(cnt > largestLen){
+				largestLen = cnt;
+			}
+		}
+	}
+    
+    return largestLen;
+
+}
+
+
+int main(){
+
+	vector<int> arr{2,6,1,9,4,5,3};
+	cout<< largestBand(arr)<<endl;
+
+	return 0;
+
+
+}
